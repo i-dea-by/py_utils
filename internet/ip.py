499a1e0b9a73955e25a6ea47a1999163545c68e7
egor daniloff <i@dea.by>
2022-05-31 13:54:22 +0300

    + internet/ip.py - with functions returning IP adresses, internal and external
    + added
    - removed
    * changed/updated

---
+++ internet/ip.py
@@ -0,0 +1,29 @@
+import urllib.request
+import socket
+
+
+def get_external_ip(host: str = '', ipv6_flag: bool = False) -> str:
+    """ Return external IP. By default use https://api.ipify.org
+        If no IPv6 address, service returning IPv4
+
+    :param host: string with name service to return ip
+    :param ipv6_flag: if True - use https://api64.ipify.org.
+    :return: String with external IP
+    """
+    if not host:
+        host = 'https://api64.ipify.org' if ipv6_flag else 'https://api.ipify.org'
+    return urllib.request.urlopen(host).read().decode('utf8')
+
+
+def get_internal_ip() -> str:
+    """ Return internal IP
+
+    :return: String with internal IP
+    """
+    return socket.gethostbyname(socket.gethostname())
+
+
+if __name__ == '__main__':
+    print(f"External IPv4: {get_external_ip()}")
+    print(f"External IPv6: {get_external_ip(ipv6_flag=True)}")
+    print(f"Internal IP: {get_internal_ip()}")
