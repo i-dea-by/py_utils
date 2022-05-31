e4228f342d3538a9474ac66e91caa2a8fd27e55b
egor daniloff <i@dea.by>
2022-05-31 13:59:52 +0300

    * ip.py - add try/except block, and update docstring
    + added
    - removed
    * changed/updated

--- internet/ip.py
+++ internet/ip.py
@@ -4,15 +4,19 @@ import socket
 
 def get_external_ip(host: str = '', ipv6_flag: bool = False) -> str:
     """ Return external IP. By default use https://api.ipify.org
-        If no IPv6 address, service returning IPv4
+        If no IPv6 address, service returning IPv4. With any exception return empty string
 
     :param host: string with name service to return ip
     :param ipv6_flag: if True - use https://api64.ipify.org.
-    :return: String with external IP
+    :return: String with external IP or empty string if any exception rised
     """
     if not host:
         host = 'https://api64.ipify.org' if ipv6_flag else 'https://api.ipify.org'
-    return urllib.request.urlopen(host).read().decode('utf8')
+    try:
+        result = urllib.request.urlopen(host).read().decode('utf8')
+    except Exception:
+        result = ''
+    return result
 
 
 def get_internal_ip() -> str:
