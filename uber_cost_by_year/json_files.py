5b43aa61d71509fad3cc720d654e4a7e56facd90
egor daniloff <i@dea.by>
2022-12-30 15:24:25 +0300

    * реформат

--- uber_cost_by_year/json_files.py
+++ uber_cost_by_year/json_files.py
@@ -1,26 +1,83 @@
+import dataclasses
 import json
-from typing import Any
+from datetime import datetime
+from pathlib import Path
+from typing import Any, TypeVar
 
-from uber_cost_by_year.typings import PathLike, _JSONencoder, _JSONdecoder
+from uber_cost_by_year.typings import RideData
+
+PathLike = TypeVar("PathLike", str, Path)
 
 
-def save_as_jsonfile(obj: Any, filename: PathLike):
+class CustomJSONencoder(json.JSONEncoder):
+    """ Класс для сериализации json """
+
+    def default(self, obj):
+        # если попался dataclass
+        if dataclasses.is_dataclass(obj):
+            return dataclasses.asdict(obj)
+        # если попался datetime
+        if isinstance(obj, datetime):
+            return obj.replace(tzinfo=None).isoformat()
+        if isinstance(obj, Path):
+            return obj.name
+        return super().default(obj)
+
+
+class CustomJSONdecoder(json.JSONDecoder):
+    """ Класс для десериализации json """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(object_hook=self.object_hook, *args, **kwargs)
+
+    @staticmethod
+    def try_datetime(value):
+        try:
+            result = datetime.fromisoformat(value)
+        except (ValueError, TypeError):
+            return False
+        return result
+
+    @staticmethod
+    def try_ride_data(value: dict):
+        # если не None значит это данные поездки
+        if value.get('ride_dt') is not None:
+            return RideData(
+                mail_to=value['mail_to'],
+                mail_from=value['mail_from'],
+                subject=value['subject'],
+                ride_dt=CustomJSONdecoder.try_datetime(value['ride_dt']),
+                ride_week_day=value['ride_week_day'],
+                ride_cost=value['ride_cost'],
+            )
+        return False
+
+    @staticmethod
+    def object_hook(obj):
+        """ Перебор объектов на предмет того, что это за структура """
+
+        if result := CustomJSONdecoder.try_ride_data(obj):
+            return result
+
+        result = {}
+        for key, value in obj.items():
+            if _value := CustomJSONdecoder.try_datetime(value):
+                result[key] = _value
+            else:
+                result[key] = value
+        return result
+
+
+def save_as_jsonfile(obj: Any, filename: PathLike, indent: int = 4):
     """ Сохраняет объект в json-файл """
+    json_str = json.dumps(obj, cls=CustomJSONencoder, ensure_ascii=False, indent=indent)
     with open(filename, mode='w', encoding='utf-8') as f:
-        f.write(to_json_str(obj))
+        f.write(json_str)
 
 
 def load_jsonfile(filename: PathLike) -> list | dict:
     """ Загружает json-файл в объект """
     with open(filename, 'r', encoding='utf-8') as f:
-        return from_json_str(f.read())
-
-
-def to_json_str(obj: Any, indent: int = 4) -> str:
-    """ Возвращает json-строку из переданного объекта на основе json.dumps """
-    return json.dumps(obj, cls=_JSONencoder, ensure_ascii=False, indent=indent)
-
-
-def from_json_str(_str: str) -> list | dict:
-    """ Декодирует json-строку в объект, на основе json.loads """
-    return json.loads(_str, cls=_JSONdecoder)
+        raw_str = f.read()
+        json_str = json.loads(raw_str, cls=CustomJSONdecoder)
+        return json_str
--- uber_cost_by_year/typings.py
+++ uber_cost_by_year/typings.py
@@ -2,12 +2,8 @@
 #               Typings and Classes             #
 #################################################
 
-import dataclasses
-import json
 from dataclasses import dataclass
 from datetime import datetime
-from pathlib import Path
-from typing import TypeVar
 
 
 @dataclass(slots=True)
@@ -20,67 +16,3 @@ class RideData:
     ride_dt: datetime
     ride_week_day: str
     ride_cost: float
-
-
-PathLike = TypeVar("PathLike", str, Path)
-
-
-# --------------- JSON serialize/deserialilze ---------------
-
-class _JSONencoder(json.JSONEncoder):
-    """ Класс для сериализации json """
-
-    def default(self, obj):
-        # если попался dataclass
-        if dataclasses.is_dataclass(obj):
-            return dataclasses.asdict(obj)
-        # если попался datetime
-        if isinstance(obj, datetime):
-            return obj.replace(tzinfo=None).isoformat()
-        if isinstance(obj, Path):
-            return obj.name
-        return super().default(obj)
-
-
-class _JSONdecoder(json.JSONDecoder):
-    """ Класс для десериализации json """
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(object_hook=self.object_hook, *args, **kwargs)
-
-    @staticmethod
-    def try_datetime(value):
-        try:
-            result = datetime.fromisoformat(value)
-        except (ValueError, TypeError):
-            return False
-        return result
-
-    @staticmethod
-    def try_ride_data(value: dict):
-        # если не None значит это данные поездки
-        if value.get('ride_dt') is not None:
-            return RideData(
-                mail_to=value['mail_to'],
-                mail_from=value['mail_from'],
-                subject=value['subject'],
-                ride_dt=_JSONdecoder.try_datetime(value['ride_dt']),
-                ride_week_day=value['ride_week_day'],
-                ride_cost=value['ride_cost'],
-            )
-        return False
-
-    @staticmethod
-    def object_hook(obj):
-        """ Перебор объектов на предмет того, что это за структура """
-
-        if result := _JSONdecoder.try_ride_data(obj):
-            return result
-
-        result = {}
-        for key, value in obj.items():
-            if _value := _JSONdecoder.try_datetime(value):
-                result[key] = _value
-            else:
-                result[key] = value
-        return result
