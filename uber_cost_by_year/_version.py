547cfe083974d353c9978d85a6a3104e07195faa
egor daniloff <i@dea.by>
2023-01-01 15:30:06 +0300

    + добавил файл версий

---
+++ uber_cost_by_year/_version.py
@@ -0,0 +1,15 @@
+"""
+    Release
+    Post-release
+    Development release
+"""
+__version_info__ = (
+    0,
+    0,
+    0
+)
+
+__version__ = '.'.join(map(str, __version_info__))
+
+__author__ = 'Egor Danilov'
+__email__ = 'i@dea.by'
