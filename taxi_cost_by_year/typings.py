f559fa6cbcd0ff0c929f11a9c5e0697adb93f5dd
egor daniloff <i@dea.by>
2023-01-01 20:07:04 +0300

    * изменил данные поездки * переделал декодер + добавил в энкодер timedelta

--- taxi_cost_by_year/typings.py
+++ taxi_cost_by_year/typings.py
@@ -4,25 +4,23 @@
 import dataclasses
 import json
 from dataclasses import dataclass
-from datetime import datetime
+from datetime import datetime, timedelta
 from pathlib import Path
 
 
 @dataclass(slots=True)
 class RideData:
-    # данные письма
-    mail_to: str
-    mail_from: str
-    subject: str
-    # данные поездки
-    ride_dt: datetime
-    ride_week_day: str
+    ride_from: str
+    ride_to: str
+    duration: timedelta | None
+    ride_dt: datetime | None
     ride_cost: float
     ride_distance: float
 
 
 class CustomJSONencoder(json.JSONEncoder):
-    """ Класс для сериализации json """
+    """ Преобразует объект json-строку
+    """
 
     def default(self, obj):
         # если попался dataclass
@@ -31,6 +29,8 @@ class CustomJSONencoder(json.JSONEncoder):
         # если попался datetime
         if isinstance(obj, datetime):
             return obj.replace(tzinfo=None).isoformat()
+        if isinstance(obj, timedelta):
+            return obj.seconds
         if isinstance(obj, Path):
             return obj.name
         return super().default(obj)
@@ -43,38 +43,22 @@ class CustomJSONdecoder(json.JSONDecoder):
         super().__init__(object_hook=self.object_hook, *args, **kwargs)
 
     @staticmethod
-    def try_datetime(value):
-        try:
-            result = datetime.fromisoformat(value)
-        except (ValueError, TypeError):
-            return False
-        return result
+    def as_ride_data(value: dict):
+        return RideData(
+            ride_from=value['ride_from'],
+            ride_to=value['ride_to'],
+            duration=timedelta(seconds=value['duration']),
+            ride_dt=datetime.fromisoformat(value['ride_dt']),
+            ride_cost=value['ride_cost'],
+            ride_distance=value['ride_distance']
+        )
 
     @staticmethod
-    def try_ride_data(value: dict):
-        # если не None значит это данные поездки
-        if value.get('ride_dt') is not None:
-            return RideData(
-                mail_to=value['mail_to'],
-                mail_from=value['mail_from'],
-                subject=value['subject'],
-                ride_dt=CustomJSONdecoder.try_datetime(value['ride_dt']),
-                ride_week_day=value['ride_week_day'],
-                ride_cost=value['ride_cost'],
-            )
-        return False
-
-    @staticmethod
-    def object_hook(obj):
+    def object_hook(obj: dict):
         """ Перебор объектов на предмет того, что это за структура """
 
-        if result := CustomJSONdecoder.try_ride_data(obj):
-            return result
+        # если в словаре есть 'ride_dt' значит пришли данные поездки
+        if 'ride_dt' in obj:
+            return CustomJSONdecoder.as_ride_data(obj)
 
-        result = {}
-        for key, value in obj.items():
-            if _value := CustomJSONdecoder.try_datetime(value):
-                result[key] = _value
-            else:
-                result[key] = value
-        return result
+        return obj
